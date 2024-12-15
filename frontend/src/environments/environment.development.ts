91f9db492950c7a10035cc1e1a5a5e4885d86034
alex_shockency <alexshockency@gmail.com>
2024-12-15 02:52:25 -0700

    update apiurls

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tekken8-leaderboard-ojdy-g88vhnvca-alex-shockencys-projects.vercel.app/api/',
+  apiUrl: 'https://tekken8-leaderboard-backend.vercel.app/api/',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tekken8-leaderboard-a67j-oi8awczzb-alex-shockencys-projects.vercel.app/api/',
+  apiUrl: 'https://tekken8-leaderboard-backend.vercel.app/api/',
 };
