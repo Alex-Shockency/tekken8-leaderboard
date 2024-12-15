7cb14b4a9576c4c461da205ddc8e34e69aa7605f
alex_shockency <alexshockency@gmail.com>
2024-12-15 02:16:16 -0700

    update api url

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://tekken8-leaderboard-a67j-oi8awczzb-alex-shockencys-projects.vercel.app/api/',
 };
