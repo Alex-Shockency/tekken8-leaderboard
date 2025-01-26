38ebf62694055e85ab1e635fc1b542d6ff54fad4
dashiel <dashiel.wellott@gmail.com>
2025-01-25 19:52:49 -0700

    add auth0 configuration. ensure basic login/logout flow

--- frontend/src/app/Components/top-nav/top-nav.component.ts
+++ frontend/src/app/Components/top-nav/top-nav.component.ts
@@ -2,25 +2,26 @@ import { Component, HostListener } from '@angular/core';
 import { RankingService } from '../../Services/ranking.service';
 import { MaterialModule } from '../../Shared/material.module';
 import { RouterModule } from '@angular/router';
+import { AuthButtonComponent } from '../auth-button/auth-button';
 
 @Component({
   selector: 'app-top-nav',
   standalone: true,
-  imports: [MaterialModule,RouterModule],
+  imports: [MaterialModule, RouterModule, AuthButtonComponent],
   templateUrl: './top-nav.component.html',
-  styleUrl: './top-nav.component.css'
+  styleUrl: './top-nav.component.css',
 })
 export class TopNavComponent {
   screenHeight = 0;
   screenWidth = 0;
-  lastUpdate: string = new Date().toLocaleString("en-US");;
+  lastUpdate: string = new Date().toLocaleString('en-US');
 
   options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
   };
 
   @HostListener('window:resize', ['$event'])
@@ -28,15 +29,13 @@ export class TopNavComponent {
     this.screenHeight = window.innerHeight;
     this.screenWidth = window.innerWidth;
   }
-  
-  constructor(private rankingService: RankingService) {
-    
-  }
+
+  constructor(private rankingService: RankingService) {}
 
   ngOnInit() {
     this.screenWidth = window.innerWidth;
-    this.rankingService.getLastUpdate().subscribe((result) =>{
-      this.lastUpdate = new Date(result.date).toLocaleString("en-US");
+    this.rankingService.getLastUpdate().subscribe((result) => {
+      this.lastUpdate = new Date(result.date).toLocaleString('en-US');
     });
   }
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,5 +7,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(),provideHttpClient()]
+  providers: [
+    provideAuth0({
+      domain: 'dev-6qq2es4t3n3j10sm.us.auth0.com',
+      clientId: 'iS8dUd5AHoeUyqMdH8hecdHkyBFeaOCP',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
 };
