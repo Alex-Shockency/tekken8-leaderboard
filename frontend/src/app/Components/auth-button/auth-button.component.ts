1f9a56080943762fbb4f56e3db150dd7dd4010c4
alex_shockency <alexshockency@gmail.com>
2025-03-30 11:09:30 -0600

    Update auth button to include your player profile

--- frontend/src/app/Components/auth-button/auth-button.component.ts
+++ frontend/src/app/Components/auth-button/auth-button.component.ts
@@ -3,6 +3,7 @@ import { AuthService, User } from '@auth0/auth0-angular';
 import { MaterialModule } from '../../Shared/material.module';
 import { DOCUMENT } from '@angular/common';
 import { Router } from '@angular/router';
+import { UserService } from '../../Services/user/user.service';
 
 @Component({
   selector: 'app-auth-button',
@@ -13,16 +14,33 @@ import { Router } from '@angular/router';
 })
 export class AuthButtonComponent {
   user: User
+  currentUser: any
+  tekkenId : string = "";
+  tekkenIdWithDases : string = "";
+
   constructor(
     @Inject(DOCUMENT) public document: Document,
-    public auth: AuthService, public router: Router
+    public auth: AuthService, public router: Router, private userService: UserService
   ) {
     this.user = auth.user$.subscribe((user) => {
       if (user) {
         this.user = user
+        this.auth.getAccessTokenSilently().subscribe((token) => {
+          this.userService.getUserById(user.sub as string, token).subscribe(
+            (result) => {
+              this.currentUser = result.playerData;
+              this.tekkenId = this.currentUser._id;
+              this.tekkenIdWithDases = this.currentUser._id.match(new RegExp('.{1,4}', 'g')).join("-");
+            },
+          );
+        });
       }
 
     })
+  }
+
+  navigateToPlayer(tekkenId:string) {
+    this.router.navigate(['/playerInfo/'+ tekkenId])
   }
 
   login() {
